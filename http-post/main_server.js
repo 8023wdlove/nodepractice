53d7d7f51af8a15b2d74a8793df382ec6f6b96f5
wangdongdong <wangdongdong@passiontec.cn>
2018-11-15 00:43:20 +0800

    综合请求服务

---
+++ http-post/main_server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const querystring = require('querystring');
+const url = require('url');
+http.createServer((req,res)=>{
+    // GET 
+    let obj = url.parse(req.url,true);
+    let path = obj.pathname;
+    let param = obj.query;
+
+    // POST
+    var str = '';
+    req.on('data',(data)=>{
+        str+=data;
+    })
+    req.on('end',()=>{
+        const POST = querystring.parse(str);
+        console.log(path,param,POST)
+        var file_name = './www'+path;
+        fs.readFile(file_name,(err,data)=>{
+            if(err){
+                res.write('NOT FOUND');
+            } else{
+                res.write(data);
+            }
+            res.end(); 
+        })
+    })
+
+}).listen(8080);
