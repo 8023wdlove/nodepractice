a69676d2aa4137b23d8e28c925b946ea12583529
wangdongdong <wangdongdong@passiontec.cn>
2018-11-17 00:53:08 +0800

    modify

--- express/server.js
+++ express/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
  var server = express();
- server.use('/',(req, res)=>{
+ server.use('/b.html',(req, res)=>{
    res.writeHead(200,{
       "Content-Type":"text/plain;charset=utf-8"
   });
-    res.write('哈哈哈哈哈哈哈');
+    res.write('b哈哈哈哈哈哈哈');
+    res.end();
+ })
+ server.use('/a.html',(req, res)=>{
+//    res.writeHead(200,{
+//       "Content-Type":"text/plain;charset=utf-8"
+//   });
+    res.send('a哈哈哈哈哈哈哈哈及斤斤计较斤斤计较哈哈');
     res.end();
  })
 
