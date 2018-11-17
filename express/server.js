3faf810d95ecbbf19a88423330d074dbe4add482
8023wdlove <1096186073@qq.com>
2018-11-17 17:21:42 +0800

    express server write way

--- express/server.js
+++ express/server.js
@@ -8,10 +8,10 @@ const express = require('express');
     res.end();
  })
  server.use('/a.html',(req, res)=>{
-//    res.writeHead(200,{
-//       "Content-Type":"text/plain;charset=utf-8"
-//   });
-    res.send({a:12,b:153}); // send 方法添加了可以直接发送对象
+   res.writeHead(200,{
+      "Content-Type":"text/plain;charset=utf-8"
+  });
+    res.write({a:12,b:153}); // send 方法添加了可以直接发送对象
     res.end();
  })
 
