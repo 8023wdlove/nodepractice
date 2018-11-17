cb7929f1d7d7aba7f440fd77f5b45a78ebcdf305
8023wdlove <1096186073@qq.com>
2018-11-17 17:19:32 +0800

    modify

--- express/server.js
+++ express/server.js
@@ -4,14 +4,14 @@ const express = require('express');
    res.writeHead(200,{
       "Content-Type":"text/plain;charset=utf-8"
   });
-    res.write('b哈哈哈哈哈哈哈');
+    res.write('b哈哈4哈哈哈哈哈');
     res.end();
  })
  server.use('/a.html',(req, res)=>{
 //    res.writeHead(200,{
 //       "Content-Type":"text/plain;charset=utf-8"
 //   });
-    res.send({a:12,b:15}); // send 方法添加了可以直接发送对象
+    res.send({a:12,b:153}); // send 方法添加了可以直接发送对象
     res.end();
  })
 
