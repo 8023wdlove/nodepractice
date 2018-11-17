00b5336109ae54abbbe0e97c04cc6129ddd09524
8023wdlove <1096186073@qq.com>
2018-11-17 17:11:03 +0800

    修正 用户名邮箱不一致的问题

---
+++ express2/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+ var server = express();
+ server.use('/b.html',(req, res)=>{
+   res.writeHead(200,{
+      "Content-Type":"text/plain;charset=utf-8"
+  });
+    res.write('b哈哈哈哈哈哈哈');
+    res.end();
+ })
+ server.use('/a.html',(req, res)=>{
+//    res.writeHead(200,{
+//       "Content-Type":"text/plain;charset=utf-8"
+//   });
+    res.send({a:12,b:15}); // send 方法添加了可以直接发送对象
+    res.end();
+ })
+
+ server.listen(8080);
