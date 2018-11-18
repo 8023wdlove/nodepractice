3aaa8125ad5dffe2c4446439525b61de2c8b4658
8023wdlove <1096186073@qq.com>
2018-11-18 23:24:31 +0800

    jade 模板加入元素属性

--- models/2.js
+++ models/2.js
@@ -1,3 +1,12 @@
 const jade = require('jade');
-var str = jade.renderFile('./views/1.jade',{pretty:true});
+const fs= require('fs');
+// var str = jade.renderFile('./views/1.jade',{pretty:true});
+var str = jade.renderFile('./views/11.jade',{pretty:true});
 console.log(str);
+fs.writeFile('./build/2.html',str,(err,data)=>{
+    if(err){
+        console.log('写入失败')
+    }else{
+        console.log('写入成功') 
+    }
+})
