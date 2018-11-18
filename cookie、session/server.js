e31af2cbceeec6300ffdfb723880d9fe33b62d77
8023wdlove <1096186073@qq.com>
2018-11-18 22:17:40 +0800

    cookies

---
+++ cookie、session/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+var server = express();
+
+// cookie
+server.use(cookieParser('xiaomiao'));
+// server.use('/',(req,res)=>{
+//     console.log(req.cookies);
+//     res.cookie('user','wangdongdong',{path:'/aaa',maxAge:30*24*3600*1000});
+//     res.send('OK');
+// })
+// server.use('/',(req,res)=>{ // cookie 加密
+//     console.log(req.signedCookies);
+//     console.log(req.cookies);
+//     req.secret ='xiaomiao'; // 秘钥
+//     res.cookie('user','wangdongdong',{signed:true}); // 签名，可以做到篡改
+//     res.send('OK');
+// })
+server.use('/',(req,res)=>{ // cookie 加密
+    console.log(req.signedCookies);
+    console.log(req.cookies);
+    res.clearCookie('user');
+    res.send('OK');
+})
+server.listen(8080);
