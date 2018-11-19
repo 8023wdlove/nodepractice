d63c417a85ff9df6455ec74714f6de26a2375b07
8023wdlove <1096186073@qq.com>
2018-11-19 23:30:42 +0800

    express整合

---
+++ expressfull/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const static = require('express-static');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const jade = require('jade');
+
+var server = express();
+
+
+
+
+server.listen(8080)
