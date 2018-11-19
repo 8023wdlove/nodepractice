b76df75b2ff2cc15fc8461c6d7dcfffe061232f2
8023wdlove <1096186073@qq.com>
2018-11-19 23:37:35 +0800

    基本1

--- expressfull/server.js
+++ expressfull/server.js
@@ -8,7 +8,24 @@ const jade = require('jade');
 
 var server = express();
 
-
-
-
 server.listen(8080)
+
+// 解析cookie
+  server.use(cookieParser('wthguihh'));
+
+
+
+  // 使用session
+  var arr = [];
+  for(var i =  0;i<1000000;i++){
+      arr.push("keys"+Math.random());
+  }
+    
+  server.use(cookieSession({name:'tttuuhihi',keys:arr,maxAge:20*3600*1000}));
+
+  // post数据
+
+  server.use(bodyParser.urlencoded({extended:false}));
+
+  // static 数据
+  server.use(static('./www'))
