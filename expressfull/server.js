ef13881c0dfa615f2fb3fb23be85dc72a1e368d2
8023wdlove <1096186073@qq.com>
2018-11-19 23:42:39 +0800

    服务器基本服务

--- expressfull/server.js
+++ expressfull/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const static = require('express-static');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');// 只能解析数据类 的post
 const ejs = require('ejs');
 const jade = require('jade');
 
@@ -10,22 +10,20 @@ var server = express();
 
 server.listen(8080)
 
-// 解析cookie
-  server.use(cookieParser('wthguihh'));
-
-
-
-  // 使用session
+// 1解析cookie
+  server.use(cookieParser('wthguihh')); 
+  // 2使用session
   var arr = [];
   for(var i =  0;i<1000000;i++){
       arr.push("keys"+Math.random());
   }
-    
   server.use(cookieSession({name:'tttuuhihi',keys:arr,maxAge:20*3600*1000}));
-
-  // post数据
-
+  // 3post数据
   server.use(bodyParser.urlencoded({extended:false}));
+  // 用户请求
+  server.use('/',(req,res,next)=>{
+      console.log(req.query,req.body,req.cookies,req.session);
+  })
 
-  // static 数据
+  // 4static 数据
   server.use(static('./www'))
