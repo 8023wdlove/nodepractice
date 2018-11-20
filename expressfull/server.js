9418646d7db1f076269c9b4da49cbd6661c05ee6
8023wdlove <1096186073@qq.com>
2018-11-20 22:11:49 +0800

    注释

--- expressfull/server.js
+++ expressfull/server.js
@@ -3,7 +3,7 @@ const static = require('express-static');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');// 只能解析数据类 的post
-const multer = require('multer');
+const multer = require('multer'); // 解析文件上传
 const ejs = require('ejs');
 const jade = require('jade');
 
@@ -18,9 +18,9 @@ server.listen(8080)
   for(var i =  0;i<1000000;i++){
       arr.push("keys"+Math.random());
   }
-  server.use(cookieSession({name:'tttuuhihi',keys:arr,maxAge:20*3600*1000}));
+  server.use(cookieSession({name:'user',keys:arr,maxAge:20*3600*1000}));
   // 3post数据
-  server.use(bodyParser.urlencoded({extended:false}));
+  server.use(bodyParser.urlencoded({extended:false})); // 解析post 请求
   server.use(multer({dest:'./www/upload'}).any())
   // 用户请求
   server.use('/',(req,res,next)=>{
