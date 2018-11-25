b1af9dbf6a0782424bd9829973eb1f6fa6714b49
8023wdlove <1096186073@qq.com>
2018-11-25 23:26:43 +0800

    返回 html

--- download/server.js
+++ download/server.js
@@ -1,22 +1,19 @@
 const express = require('express')
+const consolidate = require('consolidate');
+const ejs = require('ejs')
 const fs = require('fs')
 
 var server = express();
 server.listen(8080);
+server.engine('html',consolidate.ejs)  // 模板引擎 1、什么模板引擎 
+server.set('views','./www')   // 文件模板中在哪
+server.set('view engine','html');
 // var file_name = './www'+path;
 server.get('/index.html',(req, res)=>{
-    var name = './www'+req.url;
-    fs.readFile(name,(err,data)=>{
-      if(err){
-          res.send('error')
-      }else{
-          res.write(data)
-      }
+        res.render('index.ejs', {name:'王冬冬'})
     })
-    res.end();
-    })
-// server.get('/1.txt',(req,res)=>{
-//     res.setHeader('Content-Disposition','attachment;filename=1.txt');
-//     res.sendFile('/Users/wangdongdong/demo/nodepractice/download/www/1.txt')
-// })
+server.get('/1.txt',(req,res)=>{
+    res.setHeader('Content-Disposition','attachment;filename=1.txt');
+    res.sendFile('/Users/wangdongdong/demo/nodepractice/download/www/1.txt')
+})
 server.use(express.static('./www'))
