c249ceafc8a56d38254f24407de40bfaa5d0f589
8023wdlove <1096186073@qq.com>
2018-12-04 17:07:30 +0800

    ejs 。轮播图实现数据库取数

--- blog/server.js
+++ blog/server.js
@@ -7,38 +7,29 @@ const multer = require('multer'); // 解析文件上传
 const ejs = require('ejs');
 const consolidate = require('consolidate');
 const mysql = require('mysql');
-
 var server = express();
-
 server.listen(8080)
-
-// 1解析cookie
   server.use(cookieParser('wthguihh')); 
-  // 2使用session
   var arr = [];
   for(var i =  0;i<1000000;i++){
       arr.push("keys"+Math.random());
   }
   server.use(cookieSession({name:'user',keys:arr,maxAge:20*3600*1000}));
-  // 3post数据
-  server.use(bodyParser.urlencoded({extended:false})); // 解析post 请求
+  server.use(bodyParser.urlencoded({extended:false})); 
   server.use(multer({dest:'./www/upload'}).any())
-  // 用户请求
-//   server.use('/',(req,res,next)=>{
-//       console.log(req.query,req.body,req.cookies,req.session);
-//   })
-  // 模板引擎 1、什么模板引擎 
   server.engine('html',consolidate.ejs)
-  server.engine('excel',consolidate.jade)
-  //2、模板为件在哪 
   server.set('views','./views')
-  // 3、输出什么东西
   server.set('view engine','html');
   server.get('/',(req,res)=>{
-    //   if(req.session)
-      res.render('index.ejs', {})
+    var db =mysql.createConnection({host: 'localhost',user:'root',password:'12345678',database:'blog'});
+    db.query('SELECT * FROM `banner_table`;',(err,data)=>{
+        if(err){
+          res.status(500).send('database error').end();
+        }else {
+          console.log(data)
+          res.render('index.ejs', {banners: data})
+        }
+    })
   })
 
-
-  // 4static 数据
   server.use(static('./www'))
