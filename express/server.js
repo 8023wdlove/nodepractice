70f6c34febcdaab5dfed02022b752f6ecfc15056
8023wdlove <1096186073@qq.com>
2019-02-21 16:41:06 +0800

    qwerty

--- express/server.js
+++ express/server.js
@@ -8,11 +8,44 @@ const express = require('express');
     res.end();
  })
  server.use('/a.html',(req, res)=>{
+   console.log(3333);
 //    res.writeHead(200,{
 //       "Content-Type":"text/plain;charset=utf-8"
 //   });
-    res.send({a:12,b:15}); // send 方法添加了可以直接发送对象
+    res.send([{
+      id: '12987122',
+      name: '好滋好味鸡蛋仔',
+      category: '江浙小吃、小吃零食',
+      desc: '荷兰优质淡奶，奶香浓而不腻',
+      address: '上海市普陀区真北路',
+      shop: '王小虎夫妻店',
+      shopId: '10333'
+    }, {
+      id: '12987123',
+      name: '好滋好味鸡蛋仔',
+      category: '江浙小吃、小吃零食',
+      desc: '荷兰优质淡奶，奶香浓而不腻',
+      address: '上海市普陀区真北路',
+      shop: '王小虎夫妻店',
+      shopId: '10333'
+    }, {
+      id: '12987125',
+      name: '好滋好味鸡蛋仔',
+      category: '江浙小吃、小吃零食',
+      desc: '荷兰优质淡奶，奶香浓而不腻',
+      address: '上海市普陀区真北路',
+      shop: '王小虎夫妻店',
+      shopId: '10333'
+    }, {
+      id: '12987126',
+      name: '好滋好味鸡蛋仔',
+      category: '江浙小吃、小吃零食',
+      desc: '荷兰优质淡奶，奶香浓而不腻',
+      address: '上海市普陀区真北路',
+      shop: '王小虎夫妻店',
+      shopId: '10333'
+    }]); // send 方法添加了可以直接发送对象
     res.end();
  })
 
- server.listen(8080);
+ server.listen(8090);
