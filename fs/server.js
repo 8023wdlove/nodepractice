8a82b90553f6b1dc7f4d000c3f164f0560b37a41
8023wdlove <1096186073@qq.com>
2019-10-09 10:28:14 +0800

    add

--- fs/server.js
+++ fs/server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const fs = require("fs");
 http.createServer((req,res)=>{
   var name = './www'+req.url;
+	console.log(req.url,222)
   fs.readFile(name,(err,data)=>{
     if(err){
         res.write('error')
--- server/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-
-var server = http.createServer((req,res)=>{
-    console.log(req.url);
-    res.write("Hello Word");
-    res.end();
-    // console.log("链2接");
-}).listen(8080);
